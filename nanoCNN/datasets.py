95f24322600607ea9744743f1e45dc394af6af6b
Thomas Aarholt <thomasaarholt@gmail.com>
2020-04-03 14:33:37 +0200

    moved code from notebook

---
+++ nanoCNN/datasets.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+from generate import load_generator_pickle
+from scipy.ndimage import gaussian_filter
+import matplotlib.pyplot as plt
+
+def num_batches(total_items, batch_size):
+    return int(total_items / batch_size)
+
+def get_generator(g, gaussian_kernel=1, normalise=True, binary_labels=False):
+    def gen():
+        while True:
+            try:
+                a, b = g.__next__()
+                if normalise:
+                    a -= a.min()
+                    a /= a.max()
+                if binary_labels:
+                    b[b > 0] = 1
+                if gfilter:
+                    a = gaussian_filter(a, (gaussian_kernel, gaussian_kernel, 0))
+                yield a, b
+            except:
+                return
+    return gen
+
+def dataset_function(dataset='data', epochs=10, batch_size=32, gaussian_kernel=1, normalise=True, binary_labels=False):
+    print(f"Epochs: {epochs}\nBatch Size: {batch_size}")
+    shapes = (128, 128, 1), (128, 128, 1)
+    dataset = tf.data.Dataset.from_generator(
+        generator=get_generator(
+            load_generator_pickle(dataset)(), 
+            gaussian_kernel=gaussian_kernel, 
+            normalise=normalise, 
+            binary_labels=binary_labels
+        ),
+        output_types=(tf.float64, tf.float64),
+        output_shapes=shapes)
+    dataset = dataset.batch(batch_size).repeat(epochs)
+    return dataset
+
+def plot(*imgs):
+    fig, AX = plt.subplots(ncols=len(imgs), squeeze=False)
+    for i, img in enumerate(imgs):
+        AX[0, i].imshow(img)
+
+def plot_from_tf_dataset(dataset, i=0):
+    img, lab = dataset.as_numpy_iterator().__next__()
+    plot(img[i,...,0], lab[i,...,0])
---
+++ nanoCNN/models.py
@@ -0,0 +1,39 @@
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Input, InputLayer, Conv2D, Dropout, MaxPooling2D, concatenate, UpSampling2D
+
+def ShapesModel(classes=4, shape=(128, 128)):
+    inputs = Input(shape=(128, 128, 1))
+
+    # Encoder
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
+    conv1 = Dropout(0.2)(conv1)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D((2, 2))(conv1)
+
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Dropout(0.2)(conv2)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D((2, 2))(conv2)
+
+    # Decoder
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Dropout(0.2)(conv3)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+    conv3 = UpSampling2D((2, 2))(conv3)
+
+    up1 = concatenate([conv3, conv2], axis=-1)
+    conv4 = Conv2D(64, (3, 3), activation='relu', padding='same')(up1)
+    conv4 = Dropout(0.2)(conv4)
+    conv4 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv4)
+    conv4 = UpSampling2D((2, 2))(conv4)
+
+    up2 = concatenate([conv4, conv1], axis=-1)
+    conv5 = Conv2D(32, (3, 3), activation='relu', padding='same')(up2)
+    conv5 = Dropout(0.2)(conv5)
+    conv5 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv5)
+
+    outputs = Conv2D(classes, (1, 1) , padding='same')(conv5)
+
+    model = Model(inputs=inputs, outputs=outputs)
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    return model
