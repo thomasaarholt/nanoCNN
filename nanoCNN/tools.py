43178478468d5a7958438058cc457ef687595a4e
Thomas Aarholt <thomasaarholt@gmail.com>
2020-04-03 16:08:09 +0200

    wip

--- nanoCNN/tools.py
+++ nanoCNN/tools.py
@@ -36,13 +36,14 @@ def save_png_true_fit(name, img, fit):
     imageio.imwrite(name + "_true.png", (255*img).astype('uint8'))
     imageio.imwrite(name + "_fit.png", (255*fit).astype('uint8'))
 
-def predict_images(raw, model, split_retile=False):
+def predict_images(raw, model, split_retile=False, binary_segment=False):
     if split_retile:
         raw = split_image_into_128(raw)
     raw = normalise(raw)
     fit = model.predict(raw)
-    if split_retile:
-        fit = segment_reshape_tiled_img(fit)
-    else:
-        fit = segment_reshape_tiled_img(fit)
+    if binary_segment:
+        if split_retile:
+            fit = segment_reshape_tiled_img(fit)
+        else:
+            fit = segment(fit)
     return fit
